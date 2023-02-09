f66ee2b01771c8227c97a42aa8acb0aa7a32972c
Daniel Saulin <saulin.daniel33@gmail.com>
2023-02-09 16:34:30 +0300

    Add StructureCalculationScheme class

---
+++ RoofPitch/StructureCalculationScheme.cs
@@ -0,0 +1,16 @@
+﻿namespace RoofPitch;
+
+public class StructureCalculationScheme
+{
+    public double L { get; set; }
+    public double P { get; set; }
+    public double A_B { get; set; }
+    public double F { get; set; }
+    public double K => (A + B) / (2 * B);
+    public double RY => (1 + 2 * K) / 2;
+    public double N => 2F / L;
+    public double A => B * A_B;
+    public double B => L / 2 / (1 + A_B);
+    public double C => D * A_B;
+    public double D => F / (1 + A_B);
+}

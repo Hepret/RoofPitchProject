e985e2de3cb3faeb8772252ec83c1071c691eeff
Daniel Saulin <saulin.daniel33@gmail.com>
2023-02-11 10:35:36 +0300

    Добавил таблицу опредиления усилений

---
+++ RoofPitch/EffortCalculation.cs
@@ -0,0 +1,25 @@
+﻿namespace RoofPitch;
+
+public class EffortCalculation
+{
+    private readonly Point _point1;
+    private readonly Point _point2;
+    
+    public double X1 => _point1.X;
+    public double X2 => _point2.X;
+    public double Y1 => _point1.Y;
+    public double Y2 => _point2.Y;
+    public string Name { get; }
+    public double Length { get; }
+    public double Effort { get; }
+
+    public EffortCalculation(double p, Point point1, Point point2)
+    {
+        _point1 = point1;
+        _point2 = point2;
+
+        Name = $"{_point1.Name}-{_point2.Name}";
+        Length = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
+        Effort = Length * p;
+    }
+}
---
+++ RoofPitch/EffortCalculationTable.cs
@@ -0,0 +1,55 @@
+﻿namespace RoofPitch;
+
+public class EffortCalculationTable
+{
+    private readonly StructureCalculationScheme _scheme;
+    private Dictionary<string, Point> Points => _scheme.Points;
+    private double P => _scheme.P;
+    public List<EffortCalculation> EffortCalculationTableList { get; }
+
+    public EffortCalculationTable(StructureCalculationScheme scheme)
+    {
+        this._scheme = scheme;
+        EffortCalculationTableList = new List<EffortCalculation>();
+        EffortCalculationTableList = GetEffortCalculationTable();
+    }
+
+    private List<EffortCalculation> GetEffortCalculationTable()
+    {
+        var effortCalculationTable = new List<EffortCalculation>();
+
+        var point1 = Points["1"];
+        var point2 = Points["8"];
+        
+        effortCalculationTable.Add(new EffortCalculation(P, point1, point2));
+        
+        point1 = Points["1"];
+        point2 = Points["6"];
+        effortCalculationTable.Add(new EffortCalculation(P, point1, point2));
+        
+        point1 = Points["2"];
+        point2 = Points["6"];
+        effortCalculationTable.Add(new EffortCalculation(P, point1, point2));
+        
+        point1 = Points["3"];
+        point2 = Points["7а"];
+        effortCalculationTable.Add(new EffortCalculation(P, point1, point2));
+        
+        point1 = Points["4"];
+        point2 = Points["7б"];
+        effortCalculationTable.Add(new EffortCalculation(P, point1, point2));
+        
+        point1 = Points["5"];
+        point2 = Points["8"];
+        effortCalculationTable.Add(new EffortCalculation(P, point1, point2));
+        
+        point1 = Points["6"];
+        point2 = Points["7а"];
+        effortCalculationTable.Add(new EffortCalculation(P, point1, point2));
+        
+        point1 = Points["7б"];
+        point2 = Points["8"];
+        effortCalculationTable.Add(new EffortCalculation(P, point1, point2));
+        return effortCalculationTable;
+    }
+}
